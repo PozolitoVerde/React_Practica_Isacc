65824f772f1d5546fa0eb019d05b4ea637272bd1
PozolitoVerde <m69e10z21a@gmail.com>
2023-03-05 18:44:52 -0600

    Con login

--- src/Login.js
+++ src/Login.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 import { gapi } from "gapi-script";
 import img from "./imgs/foto.jpeg";
 import FacebookLogin from 'react-facebook-login';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 function Login() {
@@ -53,8 +55,28 @@ function Login() {
   return (
     <div>
     <div className="center">
-      
-      
+      <div className='form'>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
 
       <div class={user ? "profile" : "hidden"}>
         <img src={img} width={'250px'}  />
